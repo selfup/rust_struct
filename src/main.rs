c6b71ebc1047219133a9372b8a822d7f0426122e
selfup <boudinot.regis@yahoo.com>
2016-02-20 22:19:24 -0700

    can move up

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,20 @@
+use std::cell::Cell;
+
 struct Coordinates {
-    x: i32,
-    y: i32,
+    x: Cell<i32>,
+    y: Cell<i32>,
 }
 
 fn main() {
-    let origin = Coordinates { x: 0, y: 0 };
+    let origin = Coordinates { x: Cell::new(0), y: Cell::new(0) };
 
-    println!("X is {} and Y is {}", origin.x + 1, origin.y + 90);
+    println!("X is {:?} and Y is {:?}", origin.x, origin.y);
 
-    let mut v = vec![1, 2];
-
-    for i in &v {
-        println!("A reference to {}", i);
-    }
-
-    for i in &mut v {
-        println!("A mutable reference to {}", i);
-    }
-
-    for i in v {
-        println!("Take ownership of the vector and its element {}", i);
-    }
-
-    println!("X is {} and Y is {}", move_right(origin.x), move_up(origin.y));
-    println!("X is {} and Y is {}", move_right(origin.x), move_up(origin.y));
+    println!("X is {:?} and Y is {:?}", origin.x, move_up(origin.y));
 }
 
-fn move_up(y_cord: i32) -> i32 {
-    y_cord - 1
-}
-
-fn move_right(x_cord: i32) -> i32 {
-    x_cord + 1
+fn move_up(y_cord: Cell<i32>) -> Cell<i32> {
+    let new_val = y_cord.get();
+    y_cord.set(&new_val + 1);
+    y_cord // no semicolong here because it is the return value
 }
