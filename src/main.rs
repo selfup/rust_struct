26b2d66a50bd00404a16adc4aa19ad553f9994a7
Regis <boudinot.regis@yahoo.com>
2016-03-03 10:24:30 -0700

    can push all coords to vec

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,25 @@ fn main() {
     let mut previous_positions = vec![];
     // borrow the value so that it can change (using the '&' symbol borrows the var)
     move_up(&bike.y);
-    add_positions(&mut previous_positions, &bike.y);
+    add_positions(&mut previous_positions, &bike.y, &bike.x);
     move_down(&bike.y);
-    add_positions(&mut previous_positions, &bike.y);
+    add_positions(&mut previous_positions, &bike.y, &bike.x);
     move_right(&bike.x);
-    add_positions(&mut previous_positions, &bike.y);
+    add_positions(&mut previous_positions, &bike.y, &bike.x);
     move_left(&bike.x);
-    add_positions(&mut previous_positions, &bike.y);
+    add_positions(&mut previous_positions, &bike.y, &bike.x);
 
     println!("X is {:?}, Y is {:?}", bike.x, bike.y);
     println!("{:?}", previous_positions)
 
 }
 
-fn add_positions<'a>(vector: &'a mut Vec<i32>, y_cc: &Cell<i32>) -> &'a mut Vec<i32> {
-    let c_x = y_cc;
-    vector.push(c_x.get());
-    vector
+fn add_positions<'a>(v_cc: &'a mut Vec<i32>, y: &Cell<i32>, x: &Cell<i32>) -> &'a mut Vec<i32> {
+    let c_y = y;
+    let c_x = x;
+    v_cc.push(c_x.get());
+    v_cc.push(c_y.get());
+    v_cc
 }
 
 fn move_up(y_cord: &Cell<i32>) -> &Cell<i32> {
