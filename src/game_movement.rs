5a7ed15702058c8242a05f650e01af623c190fc0
Regis <boudinot.regis@yahoo.com>
2016-03-04 09:54:41 -0700

    breaking up some logic

---
+++ src/game_movement.rs
@@ -0,0 +1,3 @@
+pub fn combine_moves() {
+    println!("Doin stuff");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod game_movement;
+
 use std::cell::Cell;
 
 struct Coordinates {
@@ -6,6 +8,7 @@ struct Coordinates {
 }
 
 fn main() {
+    game_movement::combine_moves();
     let bike_one = Coordinates { x: Cell::new(0), y: Cell::new(50) };
     let bike_two = Coordinates { x: Cell::new(100), y: Cell::new(50) };
     let mut one_pos = vec![];
@@ -17,24 +20,28 @@ fn main() {
     add_positions(&mut one_pos, &bike_one.y, &bike_one.x);
     add_positions(&mut two_pos, &bike_two.y, &bike_two.x);
     combined_positions(&mut all_pos, &mut one_pos, &mut two_pos);
+    game_movement::combine_moves();
 
     move_down(&bike_one.y);
     move_down(&bike_two.y);
     add_positions(&mut one_pos, &bike_one.y, &bike_one.x);
     add_positions(&mut two_pos, &bike_two.y, &bike_two.x);
     combined_positions(&mut all_pos, &mut one_pos, &mut two_pos);
+    game_movement::combine_moves();
 
     move_right(&bike_one.x);
     move_left(&bike_two.x);
     add_positions(&mut one_pos, &bike_one.y, &bike_one.x);
     add_positions(&mut two_pos, &bike_two.y, &bike_two.x);
     combined_positions(&mut all_pos, &mut one_pos, &mut two_pos);
+    game_movement::combine_moves();
 
     move_left(&bike_one.x);
     move_left(&bike_two.x);
     add_positions(&mut one_pos, &bike_one.y, &bike_one.x);
     add_positions(&mut two_pos, &bike_two.y, &bike_two.x);
     combined_positions(&mut all_pos, &mut one_pos, &mut two_pos);
+    game_movement::combine_moves();
 
     println!("Bike One");
     println!("X is {:?}, Y is {:?}", bike_one.x, bike_one.y);
