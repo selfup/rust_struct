afe1426f2af84d893b34e3a0a2e828ca2ea360b3
Regis <boudinot.regis@yahoo.com>
2016-03-05 02:18:16 -0700

    removed uneeded lifetime declaration

--- src/collision.rs
+++ src/collision.rs
@@ -1,4 +1,4 @@
-pub fn alive_or_dead<'a>(o_p: &mut Vec<i32>, t_p: &mut Vec<i32>) {
+pub fn alive_or_dead(o_p: &mut Vec<i32>, t_p: &mut Vec<i32>) {
     for x in 0..o_p.len() - 1 {
         let k = x + 1;
         if t_p[x] == o_p[x] && t_p[k] == o_p[k]{
